9670f0e8d3abc38c72c60ecefaf83b7a1c95a6d1
Moises Laris Santos <3119402@live.com>
2022-04-22 16:33:35 -0500

    Added multiple endpoints

--- src/controllers/acronym.controller.ts
+++ src/controllers/acronym.controller.ts
@@ -1,18 +1,17 @@
 import { Request, Response } from 'express';
 import { AppDataSource } from './../database/index';
 import { Acronym } from '../database/entity/acronym';
-import {ILike} from "typeorm";
+import { ILike } from "typeorm";
 import { ResponseModel } from './../models/response';
 
 const acronymRepository = AppDataSource.getRepository(Acronym);
 
-
 export const getAcronym = async (req: Request, res: Response) => {
     let search = req.query.search as string;
-    let {from, limit} = req.query;
+    let { from, limit } = req.query;
     if (!from) from = '0';
     if (!limit) limit = '10';
-    if (!search) res.status(400).send('Search parameter is required.');
+    if (!search) res.status(403).send('Search parameter is required.');
 
     // ILIKE operator performs case insensitive queries.
     const [acronyms, total] = await acronymRepository.findAndCount({
@@ -23,14 +22,84 @@ export const getAcronym = async (req: Request, res: Response) => {
         skip: parseInt(from as string),
     });
 
-    res.send({acronyms, total});
+    res.send({ acronyms, total });
+}
+
+export const getAcronymById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    try {
+        const acronym = await acronymRepository.findOneById(id);
+
+        if (!acronym) {
+            return res.status(404).send({ message: `Acronym not found with the id ${id}.` } as ResponseModel);
+        }
+        return res.send({ acronym });
+    } catch (error) {
+        return res.status(403).send({ error });
+    }
+}
+
+export const getRandomAcronyms = async (req: Request, res: Response) => {
+    const amount = req.query.amount as string;
+    let numberOfAcronyms: number = 10;
+
+    if (amount && amount.trim()) {
+        numberOfAcronyms = parseInt(amount.trim());
+        if (isNaN(numberOfAcronyms)) {
+            return res.status(404).send({ error: 'Amount query param must be a number.' });
+        }
+    }
+
+    const acronyms = await AppDataSource.createQueryBuilder(Acronym, "acronym")
+        .orderBy('RANDOM()')
+        .take(numberOfAcronyms)
+        .getMany();
+
+    return res.send({ numberOfAcronyms, acronyms });
+}
+
+export const getInfo = (req: Request, res: Response) => {
+    const info = `
+    <div>
+        <h4>Welcome to the World Texting Foundation, also known as WTF.</h4>
+
+        <p>Use an Authorization header to work with your own data:</p>
+        <p>i.e: fetch(url, { headers: { 'Authorization': 'whatever-you-want' }})</p>
+      
+        <p>The following endpoints are available:</p>
+        
+        <ul>
+            <li>GET /acronym?from=50&limit=10&search=:search
+                <ul>
+                    <li>search is required</li>
+                    <li>from is defaulted by 10</li>
+                    <li>limit is defaulted by 10</li>
+                </ul>
+            </li>
+            <li>GET /acronym/info</li>
+            <li>GET /acronym/:id</li>
+            <li>GET /acronym/random?amount
+                <ul>
+                    <li>amount is defaulted by 10</li>
+                </ul>
+            </li>
+            <li>POST /acronym</li>
+            <li>PUT /acronym/:acronym</li>
+            <li>DELETE /acronym/:acronym</li>
+        </ul>
+    </div>
+
+    `;
+
+    res.send(info);
 }
 
 export const postAcronym = async (req: Request, res: Response) => {
-    const {acronym, definition} = req.body;
+    const { acronym, definition } = req.body;
 
     if (!acronym || !acronym.trim()) {
-        return res.status(400).send('Acronym must not be an empty string.');
+        return res.status(403).send('Acronym must not be an empty string.');
     }
 
     const acronymToCreate = new Acronym({
@@ -40,53 +109,67 @@ export const postAcronym = async (req: Request, res: Response) => {
 
     try {
         const acronymCreated = await acronymRepository.save(acronymToCreate);
-        res.send({message: 'Acronym created succesfully.', acronym: acronymCreated} as ResponseModel);
+        res.send({ message: 'Acronym created succesfully.', acronym: acronymCreated } as ResponseModel);
     } catch (error) {
-        res.status(400).send({error} as ResponseModel);
+        res.status(403).send({ error } as ResponseModel);
     }
 }
 
 export const updateAcronym = async (req: Request, res: Response) => {
-    const {acronym} = req.params;
-    const {definition} = req.body;
-    
+    const { acronym } = req.params;
+    const { definition } = req.body;
+
     if (!acronym || !acronym.trim()) {
-        return res.status(400).send({error: 'Acronym must not be an empty string.'} as ResponseModel);
+        return res.status(403).send({ error: 'Acronym must not be an empty string.' } as ResponseModel);
     }
 
     if (!definition || !definition.trim()) {
-        return res.status(400).send({error: 'Definition must not be an empty string.'} as ResponseModel);
+        return res.status(403).send({ error: 'Definition must not be an empty string.' } as ResponseModel);
     }
 
     const properties = await acronymRepository.findOne({
         where: {
-            acronym
-        } 
+            acronym: ILike(acronym.trim())
+        }
     });
 
     if (!properties) {
-        return res.status(400).send({error: 'Acronym was not found.'} as ResponseModel);
+        return res.status(403).send({ error: 'Acronym was not found.' } as ResponseModel);
     }
-    
+
     const updatedAcronym = await acronymRepository.save({
         ...properties,
-        definition,
+        definition: definition.trim(),
     });
-    
-    if(!updatedAcronym) {
-        return res.status(400).send({error: 'Definition was not updated.'} as ResponseModel);
+
+    if (!updatedAcronym) {
+        return res.status(403).send({ error: 'Definition was not updated.' } as ResponseModel);
     }
 
     return res.send({
-        message: `Acronym "${properties.acronym}" was sucessfully updated.`,
+        message: `Acronym '${properties.acronym}' was sucessfully updated.`,
         acronym: updatedAcronym,
     } as ResponseModel);
 }
 
 export const deleteAcronym = async (req: Request, res: Response) => {
-    const {acronym} = req.params;
-    
+    const { acronym } = req.params;
+
     if (!acronym || !acronym.trim()) {
-        return res.status(400).send({error: 'Acronym must not be an empty string.'} as ResponseModel);
+        return res.status(403).send({ error: 'Acronym must not be an empty string.' } as ResponseModel);
     }
+
+    try {
+        const deletedAcronym = await acronymRepository.delete({
+            acronym: ILike(acronym)
+        });
+        if (!deletedAcronym || !deletedAcronym.affected || deletedAcronym.affected <= 0) {
+            return res.send({ message: `Acronym '${acronym}' was not found.` } as ResponseModel);
+        }
+    } catch (error) {
+        return res.status(403).send({ message: `Acronym '${acronym}' was not deleted.`, error } as ResponseModel);
+    }
+
+
+    return res.send({ message: `Acronym '${acronym}' succesfully deleted.` } as ResponseModel);
 }
--- src/routes/acronym.routes.ts
+++ src/routes/acronym.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getAcronym, postAcronym, updateAcronym, deleteAcronym } from './../controllers/acronym.controller';
+import { getAcronym, postAcronym, updateAcronym, deleteAcronym, getAcronymById, getRandomAcronyms, getInfo } from './../controllers/acronym.controller';
 
 const router = Router();
 router.get('/', getAcronym);
@@ -7,6 +7,10 @@ router.post('/', postAcronym);
 router.put('/:acronym', updateAcronym);
 router.delete('/:acronym', deleteAcronym);
 
+// Extra endpoints
+router.get('/random', getRandomAcronyms);
+router.get('/info', getInfo);
+router.get('/:id', getAcronymById);
 // Other routes return 404 status
 router.get("*", (req, res) => {
   res.sendStatus(404);
